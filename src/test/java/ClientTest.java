4c6f1cef87fe7385ba5554b9cc31b821a160a4d3
Christopher Douglass <christopher.d.douglass@gmail.com>
2020-07-21 14:57:00 -0700

    Added pom, unit tests file, and changed previous test file name to IntegrationTest

---
+++ src/test/java/ClientTest.java
@@ -0,0 +1,45 @@
+import Client.Client;
+import net.schmizz.sshj.SSHClient;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.io.IOException;
+
+import static junit.framework.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+
+public class ClientTest {
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testConnect() throws IOException {
+//        when(client.connect(anyString(), anyString(), anyString(), anyInt()))
+//            .thenReturn(new SSHClient());
+
+        final SSHClient sshClient = mock(SSHClient.class);
+
+        doNothing().when(sshClient).loadKnownHosts();
+        doNothing().when(sshClient).connect(anyString(), anyInt());
+        doNothing().when(sshClient).authPassword(anyString(), anyString());
+        Client c = new Client() {
+            @Override
+            protected SSHClient createSSHClient() {
+                return sshClient;
+            }
+        };
+
+        SSHClient client = c.connect("1", "2", "3", 4);
+
+        assertNotNull(client);
+//        verify(client).connect("1", "2", "3", 4);
+
+        // Make the ssh.connect a thing in the interface, then use 'when' on like client.connect() to return the ssh
+    }
+}
--- src/test/java/IntegrationTests.java
+++ src/test/java/IntegrationTests.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 import static org.junit.Assert.assertTrue;
 
-public class UnitTests {
+public class IntegrationTests {
     private Client client = new Client();
 
     @Test
